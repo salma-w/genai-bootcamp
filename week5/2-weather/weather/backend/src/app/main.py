e6787a30f77e8d5740c31a63ab4722b3e88f41de
salma-w <salma.wahwah@yahoo.com>
2025-10-29 18:04:48 -0700

    add

--- week5/2-weather/weather/backend/src/app/main.py
+++ week5/2-weather/weather/backend/src/app/main.py
@@ -135,3 +135,4 @@ async def generate(agent: Agent, session_id: str, prompt: str, request: Request)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
+
